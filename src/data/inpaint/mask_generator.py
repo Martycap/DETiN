530515f8276948e4a2ae0eedac79075e8b63b7b2
donatooooooo <donatoboccuzzi@gmail.com>
2025-05-22 19:46:15 +0200

    enumerate boxes

--- src/data/inpaint/mask_generator.py
+++ src/data/inpaint/mask_generator.py
@@ -21,7 +21,7 @@ class Mask:
         return mask
 
 
-    def bbox_mask(coco, image_id, size=(256, 256), num_boxes=3):
+    def bbox_mask(coco, image_id, size=(256, 256)):
         """
         Crea una maschera binaria contenente i rettangoli delle bounding box per un'immagine
         """
@@ -32,7 +32,7 @@ class Mask:
         img_info = coco.loadImgs(image_id)[0]
         height, width = img_info['height'], img_info['width']
 
-        selected = anns[:num_boxes]
+        selected = anns[:3]
         for ann in selected:
             x, y, w, h = ann['bbox']
             box_mask = np.zeros((height, width), dtype=np.uint8)
@@ -45,12 +45,12 @@ class Mask:
         return mask
 
 
-    def random_box_mask(size=(512, 512)):
+    def random_box_mask(size=(256, 256)):
         """
         Genera una maschera contentente da 3 a 8 rettangoli casuali
         """
         mask = np.zeros(size, dtype=np.uint8)
-        for _ in range(random.randint(3, 8)):
+        for _ in range(random.randint(1, 3)):
             x1 = random.randint(0, size[1] - 1)
             y1 = random.randint(0, size[0] - 1)
             x2 = random.randint(x1, size[1])
@@ -77,33 +77,3 @@ def check_ratio(mask):
     if white_ratio > RATIO:
         raise WhiteMask()
     return
-
-# -------------------------------------------------------------------------
-# from pycocotools.coco import COCO
-# import os, matplotlib.pyplot as plt
-
-# dataset = COCO("data/raw/annotations/instances_val2017.json")
-# img_ids = dataset.getImgIds()
-# random_id = random.choice(img_ids)
-# img_info = dataset.loadImgs(random_id)[0]
-
-# img_path = os.path.join("data/raw/val_images", img_info['file_name'])
-# image = cv2.imread(img_path)
-# image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# image_resized = cv2.resize(image_rgb, (256, 256), interpolation=cv2.INTER_AREA)
-
-# mask = segmentation_mask(dataset, img_info["id"])
-
-# plt.figure(figsize=(10, 5))
-# plt.subplot(1, 2, 1)
-# plt.title("Immagine COCO")
-# plt.imshow(image_resized)
-# plt.axis("off")
-
-# plt.subplot(1, 2, 2)
-# plt.title("Maschera di Segmentazione")
-# plt.imshow(mask, cmap="gray")
-# plt.axis("off")
-
-# plt.tight_layout()
-# plt.show()
