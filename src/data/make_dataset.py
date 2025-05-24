fdfbc822316946a13e03b7ae2a55a44afd9f7dbb
donatooooooo <donatoboccuzzi@gmail.com>
2025-05-24 18:10:48 +0200

    lazy image loader

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,81 +1,77 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import random, sys, os
+sys.path.append(os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..")))
 from data.inpaint.inpaint_models import Inpaint
 from data.inpaint.mask_generator import Mask
 from pycocotools.coco import COCO
-import random, cv2, os
+from PIL import Image
 
-def center_crop(image, size = 256):
+def center_crop(image, size=256):
     """
-    Returns the image cropped to a centered square,
-    then resized to preserve aspect ratio and avoid distortion.
+    Crops the largest possible centered square from the image,
+    then resizes it to (size, size) for CNN input.
     """
-    
-    height, width, _ = image.shape
-    min_dim = min(height, width)
+    width, height = image.size
+    min_dim = min(width, height)
 
-    top = (height - min_dim) // 2
     left = (width - min_dim) // 2
-    cropped = image[top:top+min_dim, left:left+min_dim]
+    top = (height - min_dim) // 2
+    right = left + min_dim
+    bottom = top + min_dim
 
-    resized = cv2.resize(cropped, (size, size), interpolation=cv2.INTER_LANCZOS4)
+    cropped = image.crop((left, top, right, bottom))
+    resized = cropped.resize((size, size), Image.LANCZOS)
     return resized
 
-def getImages():
-    """
-    Retrievs images from the folder.
-    """
-    
-    folder = "./data/raw/val_images"
-    images = []
-    for image in os.listdir(folder):
-        path = os.path.join(folder, image)
-        image = cv2.imread(path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        resized = center_crop(image)
-        images.append(resized)
-    return images
 
-def prompt_generator(dataset, index, selections):
-    parts = []
-    for _ in range(selections - 1):
-        id = random.choice(index)
-        annotations = dataset.loadAnns(dataset.getAnnIds(imgIds=id))
-        part = random.choice(annotations)
-        parts.append(part['caption'].rstrip('. ').strip())
-    return " and ".join(parts)
+def image_loader(dataset: COCO, index, folder="./data/raw/val_images"):
+    """
+    Lazy image loader based on the IDs provided by COCO dataset.
+    """
 
+    for id in index:
+        info = dataset.loadImgs([id])[0]
+        file_name = info["file_name"]
+        file_path = os.path.join(folder, file_name)
+        try:
+            img = Image.open(file_path).convert("RGB")
+            resized = center_crop(img)
+            yield (resized, id, file_name)
+        except Exception as e:
+            print(f"Error with image: {file_name} -> {e}")
+    return
 
 if __name__ == "__main__":
-    dataset = COCO("data/raw/annotations/captions_val2017.json")
+    dataset = COCO("data/raw/annotations/instances_val2017.json")
     index = dataset.getImgIds()
+
+    random.shuffle(index)
+    for image, id, file_name in image_loader(dataset, index):
+        mask = Mask.segmentation_mask(dataset, id)
+        mask_pil = Image.fromarray(mask).convert("L")
+        
+        image.show()
+        mask_pil.show()
+
+        break
     
-    prompt = prompt_generator(dataset, index, 3)
-    print(prompt)
-    images = getImages()
+    # images[256].show()
     
-    import matplotlib.pyplot as plt
-    plt.imshow(images[342])
-    plt.axis('off')
-    plt.show()
+    # mask = Mask.random_box_mask()
     
-    mask = Mask.random_box_mask()
+    # models = Inpaint("./models/inpaint")
     
-    models = Inpaint("./models/inpaint")
-    
-    from PIL import Image
-    image = Image.fromarray(images[342])
-    mask = Image.fromarray(mask).convert("L")
-    plt.imshow(mask, cmap = "gray")
-    plt.axis('off')
-    plt.show()
+    # from PIL import Image
+    # image = Image.fromarray(images[342])
+    # mask = Image.fromarray(mask).convert("L")
+    # plt.imshow(mask, cmap = "gray")
+    # plt.axis('off')
+    # plt.show()
     
     
-    output = models.inference_kandinsky(prompt, images, mask)
+    # output = models.inference_kandinsky(prompt, images, mask)
     
-    output.show()
+    # output.show()
     
     # img_id = random.choice(index)
     # img_info = dataset.loadImgs(img_id)[0]
