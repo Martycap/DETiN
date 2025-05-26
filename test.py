6bc42cd22bd904bfa8120a2e66ca062348ee9073
Martina Capone <mcapone@cybsec.it>
2025-05-27 00:00:19 +0200

    Delete unused or redundant files

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from utils.dataset import create_triplets_from_tampered
-
-
-print("Tampered images:", len(os.listdir("data/raw/CASIA2/Tampered")))
-print("Mask images:", len(os.listdir("data/raw/CASIA2/Masks")))
-print("Authentic images:", len(os.listdir("data/raw/CASIA2/Authentic")))
-triplets = create_triplets_from_tampered("data/raw/CASIA2/Tampered", "data/raw/CASIA2/Masks", "data/raw/CASIA2/Authentic")
-print(f"Totale triplets trovati: {len(triplets)}")
