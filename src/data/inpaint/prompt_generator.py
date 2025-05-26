b3c0874f04ca6813d6bbbc2eef9dc8b82c97f297
Donatooooooo <donatoboccuzzi@gmail.com>
2025-05-26 18:14:42 +0200

    just a single prompt

--- src/data/inpaint/prompt_generator.py
+++ src/data/inpaint/prompt_generator.py
@@ -2,21 +2,29 @@ import random, json
 from pycocotools.coco import COCO
 
 def generator(dataset, index):
+    """
+    Prompt generator by COCO's captions.
+    """
+    
     parts = []
-    for _ in range(2):
+    for _ in range(1):
         id = random.choice(index)
         annotations = dataset.loadAnns(dataset.getAnnIds(imgIds=id))
         part = random.choice(annotations)
         parts.append(part['caption'].rstrip('. ').strip())
-    return " and ".join(parts)
+    return part['caption'].lower()
 
 def save_prompts():
+    """
+    Save prompts generated in a json file.
+    """
+    
     prompts = set()
     dataset = COCO("data/raw/annotations/captions_val2017.json")
     index = dataset.getImgIds()
     
     for _ in range(5000):
-        prompt = generator(dataset, index).lower()
+        prompt = generator(dataset, index)
         prompts.add(prompt)
     
     with open("./data/raw/prompts.json", "w", encoding="utf-8") as file:
@@ -24,8 +32,11 @@ def save_prompts():
     return
         
 def load_prompts():
+    """
+    Prompts loader from json file.
+    """
+    
     file_path = "./data/raw/prompts.json"
     with open(file_path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-        
+        data = json.load(f)  
     return data
